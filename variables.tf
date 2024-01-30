54e7944ba5d57e63befe49db874790b2c3b6b730
priyanshi gola <priyanshigola8@gmail.com>
2024-01-30 18:21:01 +0530

    fourth commit

--- variables.tf
+++ variables.tf
@@ -37,3 +37,44 @@ variable "region" {
   type = string
   description = "region"
 }
+
+### s3 bucket
+variable "bucket" {
+  type = string
+  description = "Name of the S3 bucket"
+}
+
+variable "block_public_acls" {
+  type = bool
+  description = "Permission to block the public acls "
+}
+
+variable "block_public_policy" {
+  type = bool
+  description = "Permission to block the public policy "
+}
+
+variable "ignore_public_acls" {
+  type = bool
+  description = "Permission to ignore the public acls "
+}
+
+variable "restrict_public_buckets" {
+  type = bool
+  description = "Permission to restrict the public buckets "
+}
+
+variable "versioning_configuration" {
+  type = string
+  description = "bucket versioning "
+}
+
+variable "Environment" {
+  type = string
+  description = "tag"
+}
+
+variable "s3_tags" {
+  type = string
+  description = "tag"
+}
