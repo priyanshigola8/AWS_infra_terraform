34e220998bee089afaa075e3068496b7b5bb4d1a
priyanshi gola <priyanshigola8@gmail.com>
2024-03-06 01:51:21 +0530

    updates

--- ec2.tf
+++ ec2.tf
@@ -13,6 +13,6 @@ resource "aws_vpc" "test_vpc" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.test_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "172.16.20.0/24"
   availability_zone = "us-east-1a"
 }
