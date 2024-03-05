cbb5a076b90e282a48b2a5880a5d18d2df111fa5
priyanshi gola <priyanshigola8@gmail.com>
2024-03-06 01:41:28 +0530

    updates

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,18 @@
-# resource "aws_instance" "test-1" {
-#   ami           = var.ami
-#   instance_type = var.instance_type
-#   subnet_id = aws_subnet.public_subnet.id
-#   tags = {
-#     Name = var.tags
-#   }
-# }
+resource "aws_instance" "test-1" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.public_subnet.id
+  tags = {
+    Name = var.tags
+  }
+}
 
-# resource "aws_vpc" "test_vpc" {
-#   cidr_block = var.cidr_block
-# }
+resource "aws_vpc" "test_vpc" {
+  cidr_block = var.cidr_block
+}
 
-# resource "aws_subnet" "my_subnet" {
-#   vpc_id            = aws_vpc.test_vpc.id
-#   cidr_block        = "172.16.10.0/24"
-#   availability_zone = "us-east-1a"
-# }
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.test_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+}
